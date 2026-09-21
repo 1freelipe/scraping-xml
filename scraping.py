dde0e95585c0e4c29e88d227b8100f28b348601a
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-09-21 12:21:04 -0300

    feat: adicionando uma confirmação de arquivo, antes de sobrescrever o CSV existente.

--- scraping.py
+++ scraping.py
@@ -3,11 +3,12 @@ import csv
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 
-yesterday = (datetime.now() - timedelta(days=2)).strftime("%Y-%m-%d")
+yesterday = (datetime.now() - timedelta(days=21)).strftime("%Y-%m-%d")
 
 # Configuração de pastas
 PASTA_XML = r"C:\XMLCTE"
 ARQUIVO_SAIDA = fr"C:\Users\felipe.rodrigues\python-portable\extract_keys\keys_extracts_{yesterday}.csv"
+PATH_NAME = os.path.basename(ARQUIVO_SAIDA).lstrip('keys_extracts_')
 
 # Namespace padrão da SEFAZ para CT-e
 NAMESPACE = {'ns': 'http://www.portalfiscal.inf.br/cte'}
@@ -20,12 +21,13 @@ def extract_data():
         if ctes.lower().endswith('.xml'):
             folder_path = os.path.join(PASTA_XML, ctes)
 
+
             try:
                 # Ler toda a árvore do XML
                 tree = ET.parse(folder_path)
                 root = tree.getroot()
 
-                # Buscar o número do CT-e (Tag <nCT>)
+                # Buscar o número do CT-e (Tag <nCT>) 
                 tag_nct = root.find('.//ns:nCT', NAMESPACE)
                 numero_cte = tag_nct.text if tag_nct is not None else "Sem número"
 
@@ -44,6 +46,20 @@ def extract_data():
             except Exception as e:
                 print(f"Erro inesperado no arquivo {ctes}: {e}")
 
+    # Verificar se o arquivo já existe
+    if os.path.exists(ARQUIVO_SAIDA):
+        print("\nATENÇÃO!")
+        print(f"O arquivo já existe:")
+        print(PATH_NAME)
+
+        response = input("Deseja sobrescrever o arquivo? (S/N)").strip().upper()
+
+        if response != "S":
+            print("\nOperação cancelada.")
+            print("O arquivo existente foi preservado.")
+            print("Os arquivos XML também NÃO foram apagados")
+            return False
+
     # Gerando CSV para os arquivos extraídos
     with open(ARQUIVO_SAIDA, mode='w', newline='', encoding='utf-8') as csv_file:
         columns = ['Numero_CTE', 'Chave_NFE']
@@ -54,7 +70,9 @@ def extract_data():
             writer.writerow(line)
 
     print(f"Sucesso! Extração concluída. Total de notas encontradas: {len(data)}")
-    print(f"Arquivo salvo em: {ARQUIVO_SAIDA}")
+    print(f"Arquivo salvo como em: {PATH_NAME}")
+
+    return True
 
 # Apagar todos os arquivos da pasta pós extração
 def clear_xml_folder():
@@ -71,6 +89,8 @@ def clear_xml_folder():
 
 # Execute script
 if __name__ == "__main__":
-    extract_data()
-    clear_xml_folder()
+    sucess = extract_data()
+
+    if sucess:
+        clear_xml_folder()
 
